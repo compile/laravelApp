35a5ee0e90b8348fa80f79ca17eb47a0bb3e1326
50 <3920699@qq.com>
2019-03-12 15:40:28 +0800

    缩略图表～

--- database/factories/ThumbFactory.php
+++ database/factories/ThumbFactory.php
@@ -1,9 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
-
+//缩略图片表 thumb   path  status
 $factory->define(App\Models\Thumb::class, function (Faker $faker) {
     return [
         // 'name' => $faker->name,
+        'path' => $faker->imageUrl(100,100),
+        'status' => rand(0,1)
     ];
 });
