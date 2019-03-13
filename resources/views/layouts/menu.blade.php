21287750e401bcfc144f9e987c25d46b45f588ce
50 <3920699@qq.com>
2019-03-13 15:07:52 +0800

    菜单名称用英文

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,21 +1,21 @@
 <div class="col-md-2" style="float:left;">
 
     <dd>
-    <dt>视频</dt>
-    <dl><a href="{{ route('works.index') }}">视频列表</a></dl>
-    <dl><a href="{{ route('works.create') }}">视频添加</a></dl>
-    <dt>作者</dt>
-    <dl><a href="{{ route('authors.index') }}">作者列表</a></dl>
-    <dl><a href="{{ route('authors.create') }}">作者添加</a></dl>
+    <dt>video</dt>
+    <dl><a href="{{ route('works.index') }}">video list</a></dl>
+    <dl><a href="{{ route('works.create') }}">video add</a></dl>
+    <dt>authors</dt>
+    <dl><a href="{{ route('authors.index') }}">author list</a></dl>
+    <dl><a href="{{ route('authors.create') }}">author add</a></dl>
 
-    <dt>频道</dt>
-    <dl><a href="{{ route('channels.index') }}">频道列表</a></dl>
-    <dl><a href="{{ route('channels.create') }}">频道添加</a></dl>
+    <dt>channel</dt>
+    <dl><a href="{{ route('channels.index') }}">channel list</a></dl>
+    <dl><a href="{{ route('channels.create') }}">channel add</a></dl>
     </dd>
 
-    <dt>缩略图</dt>
-    <dl><a href="{{ route('thumbs.index') }}">缩略图列表</a></dl>
-    <dl><a href="{{ route('thumbs.create') }}">缩略图添加</a></dl>
+    <dt>thumb</dt>
+    <dl><a href="{{ route('thumbs.index') }}">thumb list</a></dl>
+    <dl><a href="{{ route('thumbs.create') }}">thumb add</a></dl>
     </dd>
 
 
